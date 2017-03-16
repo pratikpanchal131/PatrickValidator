e46729c551c4627b09b142644346b14a5a952f7e
Pratik Panchak <pratik.panchal@indianic.com>
2017-03-16 14:43:58 +0530

    Pod Create Initial

---
+++ Example/Pods/Target Support Files/PatrickValidator/PatrickValidator-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_PatrickValidator : NSObject
+@end
+@implementation PodsDummy_PatrickValidator
+@end
---
+++ Example/Pods/Target Support Files/Pods-PatrickValidator_Example/Pods-PatrickValidator_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PatrickValidator_Example : NSObject
+@end
+@implementation PodsDummy_Pods_PatrickValidator_Example
+@end
